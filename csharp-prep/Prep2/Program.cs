da7d7ff3aef8b217f6e9a2884bdc3ea843503d44
Seth Ricks <slricks340@byui.edu>
2023-04-19 08:59:31 -0600

    finished the assignment!

--- csharp-prep/Prep2/Program.cs
+++ csharp-prep/Prep2/Program.cs
@@ -5,6 +5,80 @@ class Program
     static void Main(string[] args)
     {
         //This is prep 2
-        Console.WriteLine("Hello Prep2 World!");
+        Console.Write("What is your grade percentage? ");
+        string userInput = Console.ReadLine();
+        int grade = int.Parse(userInput);
+        string letter = "";
+        string sign = "";
+
+        if (grade >= 90)
+        {
+            letter = "A";
+        }
+
+        else if (grade >= 80)
+        {
+            letter = "B";
+        }
+
+        else if (grade >= 70)
+        {
+            letter = "C";
+        }
+
+        else if (grade >= 60)
+        {
+            letter = "D";
+        }
+
+        else if (grade < 60)
+        {
+            letter = "F";
+        }
+
+
+
+        if (grade >= 70)
+        {
+            Console.WriteLine("You're passing the course! ");
+        }
+
+        else if (grade < 70)
+        {
+            Console.WriteLine("You're not passing the course. ");
+        }
+
+        if (letter == "A")
+        {
+            if (grade % 10 >= 7)
+            {
+                sign = "";
+            }
+
+            else if (grade % 10 < 7)
+            {
+                sign = "-";
+            }
+        }
+
+        else if (letter == "F")
+        {
+            sign = "";
+        }
+
+        else
+        {
+            if (grade % 10 >= 7)
+            {
+                sign = "+";
+            }
+
+            else if (grade % 10 < 7)
+            {
+                sign = "-";
+            }
+        }
+
+        Console.WriteLine($"Your letter grade is {letter}{sign}");
     }
 }
