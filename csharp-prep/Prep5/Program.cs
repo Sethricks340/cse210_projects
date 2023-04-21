c1576cf717a36f43d6d87d38967137f13a8a0402
Seth Ricks <slricks340@byui.edu>
2023-04-21 14:47:23 -0600

    Finished the assignment for prep 5

--- csharp-prep/Prep5/Program.cs
+++ csharp-prep/Prep5/Program.cs
@@ -4,6 +4,40 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep5 World!");
+        DisplayWelcome();
+        string name = PromptUserName();
+        float number = PromptUserNumber();
+        float squareNumber = SquareNumber(number);
+        DisplayResult(name, squareNumber);
     }
+
+    static void DisplayWelcome()
+    {
+        Console.WriteLine("Welcome to the program! ");
+    }
+
+    static string PromptUserName()
+    {
+        Console.Write("Please enter your name: ");
+        string name = Console.ReadLine();
+        return name;
+    }
+    static float PromptUserNumber()
+    {
+        Console.Write("Please enter your favorite number: ");
+        float number = float.Parse(Console.ReadLine());
+        return number;
+    }
+
+    static float SquareNumber(float number)
+    {
+        float squareNumber = number * number;
+        return squareNumber;
+    }
+
+    static void DisplayResult(string name, float squareNumber)
+    {
+        Console.WriteLine($"{name}, the square of your number is: {squareNumber}");
+    }
+
 }
