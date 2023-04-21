e0ddd1cf7e1b95e663a4d4af78e530b97a94765c
Seth Ricks <slricks340@byui.edu>
2023-04-20 19:26:01 -0600

    Finished the assignment, including the stretch challenges

--- csharp-prep/Prep3/Program.cs
+++ csharp-prep/Prep3/Program.cs
@@ -4,6 +4,39 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep3 World!");
-    }
+    Random randomGenerator = new Random();
+    int guess;
+    int count;
+    string response = "yes";
+    int number = -1;
+
+while (response == "yes")
+{
+number = randomGenerator.Next(1, 101);
+count = 0;
+    do
+    {
+        Console.Write("What is your guess? ");
+        guess = int.Parse(Console.ReadLine());
+
+
+        if (guess > number)
+        {
+            Console.WriteLine("Lower");
+        }
+
+        else if (guess < number)
+        {
+            Console.WriteLine("Higher");
+        }
+    count += 1;
+    } while (number != guess);
+
+    Console.WriteLine("You guessed it! ");
+    Console.WriteLine($"It took you {count} guesses! ");
+    Console.Write("Do you want to continue? ");
+    response = Console.ReadLine();
+
+}
+    } 
 }
