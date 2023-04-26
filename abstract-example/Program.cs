10fa18175d2a798e68d0d16162ecd8a6de31caa6
Seth Ricks <slricks340@byui.edu>
2023-04-26 12:29:16 -0600

    This is what we did in class 4/26/2023

---
+++ abstract-example/Program.cs
@@ -0,0 +1,12 @@
+﻿// See https://aka.ms/new-console-template for more information
+//Console.WriteLine("Hello, World!");
+
+Book mistborn = new Book("Mistborn", "Brandon Sanderson");
+
+mistborn.Display();
+
+Book theBookOfMormon = new Book("The Book of Mormon", "God");
+
+theBookOfMormon.Display();
+
+Console.WriteLine(mistborn.IsAvailable());
