e53ec1a7f69a72a4233c2c58a6d014e4ef0cc560
Seth Ricks <slricks340@byui.edu>
2023-05-04 12:09:48 -0600

    Pre- made the diagram.

---
+++ prove/Develop02/prompt.cs
@@ -0,0 +1,21 @@
+public class prompt
+{
+    List<string> _usedPrompts = new List<string>();
+    List<string> _prompts = new List<string>();
+
+    public prompt()
+    {
+        _prompts.Add("How was your day? ");
+    } 
+
+
+    public string GetRandomPrompt()
+    {
+        return "This is a prompt";
+    }
+
+
+
+
+
+}
