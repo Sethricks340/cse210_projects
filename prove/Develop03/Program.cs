2213a90a6307d85daa85783c4eac584118ebea86
Seth Ricks <slricks340@byui.edu>
2023-05-23 18:19:22 -0600

    Added exceeding requirements comments

--- prove/Develop03/Program.cs
+++ prove/Develop03/Program.cs
@@ -4,7 +4,10 @@ class Program
 {
     static void Main(string[] args)
     {
+        //To exceed requirements:
         //The scriptures and references can be set by hand, if not then a random scripture is generated.
+        //Random words are chosen only from those that have not already been erased.
+
         // Reference reference = new Reference("2 Nephi", "2", "27");
         // Scripture scripture = new Scripture("Wherefore, men are free according to the flesh; and all things are given them which are expedient unto man. And they are free to choose liberty and eternal life, through the great Mediator of all men, or to choose captivity and death, according to the captivity and power of the devil; for he seeketh that all men might be miserable like unto himself.");
         Scripture scripture = new Scripture();
