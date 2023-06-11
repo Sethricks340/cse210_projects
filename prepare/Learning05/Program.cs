da554e562786ed711f3e564a33fadce95624d5b5
Seth Ricks <slricks340@byui.edu>
2023-06-10 18:03:02 -0600

    Made some very small changes to the comments at the end of the program class

--- prepare/Learning05/Program.cs
+++ prepare/Learning05/Program.cs
@@ -18,17 +18,18 @@ class Program
 
 
 
-        // Square square = new Square(4);
+
+        // Square square = new Square(4, "purple");
         // square.SetColor("purple");
         // Console.WriteLine(square.GetColor());
         // Console.WriteLine(square.GetArea());
         
-        // Rectangle rectangle = new Rectangle(4 ,7);
+        // Rectangle rectangle = new Rectangle(4 ,7, "red");
         // rectangle.SetColor("red");
         // Console.WriteLine(rectangle.GetColor());
         // Console.WriteLine(rectangle.GetArea());
 
-        // Circle circle = new Circle(4);
+        // Circle circle = new Circle(4, "blue");
         // circle.SetColor("blue");
         // Console.WriteLine(circle.GetColor());
         // Console.WriteLine(circle.GetArea());
