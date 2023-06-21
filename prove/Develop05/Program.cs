03c8f7a0ca4e8694aae0eb2538ab21e3880c525f
Seth Ricks <slricks340@byui.edu>
2023-06-21 16:38:08 -0600

    fixed a typo

--- prove/Develop05/Program.cs
+++ prove/Develop05/Program.cs
@@ -3,7 +3,7 @@ using System;
 class Program
 {
 
-    //To exceed the requirements in this program, the user is not able to quit unless they first save. Also, ny order of creating, listing, saving, and loading can be done without
+    //To exceed the requirements in this program, the user is not able to quit unless they first save. Also, any order of creating, listing, saving, and loading can be done without
     //any goals being lost. For example, if the user creates a goal and then tries to load a file, the goal they created will be added to the file that they load. 
 
     static void Main(string[] args)
