f8f53a4f0d91761e78073adcf38c0b3720159420
Seth Ricks <slricks340@byui.edu>
2024-03-17 15:09:41 -0600

    izzy project

---
+++ CountryGuess/Program.cs
@@ -0,0 +1,82 @@
+using System;
+using System.Drawing;
+using System.Windows.Forms;
+
+namespace CountryGuessGame
+{
+    public partial class MainForm : Form
+    {
+        private PictureBox countryPictureBox;
+        private TextBox guessTextBox;
+        private Button checkButton;
+
+        private string[] countries = { "France", "Germany", "Italy" }; // Add more countries as needed
+        private string[] countries_link = {@"Screenshot France.png", @"Screenshot Germany.png", @"Screenshot italy.png"};
+        private Random random = new Random();
+        private int currentCountryIndex;
+
+        public MainForm()
+        {
+            InitializeComponent();
+            LoadNextCountry();
+        }
+
+        private void InitializeComponent()
+        {
+            this.Text = "Country Guessing Game";
+            this.Size = new Size(4000, 3000);
+
+            countryPictureBox = new PictureBox();
+            countryPictureBox.Dock = DockStyle.Top;
+            countryPictureBox.SizeMode = PictureBoxSizeMode.AutoSize;
+            this.Controls.Add(countryPictureBox);
+
+            guessTextBox = new TextBox();
+            guessTextBox.Dock = DockStyle.Top;
+            this.Controls.Add(guessTextBox);
+
+            checkButton = new Button();
+            checkButton.Text = "Check Guess";
+            checkButton.Dock = DockStyle.Top;
+            checkButton.Click += CheckButton_Click;
+            this.Controls.Add(checkButton);
+        }
+
+        private void LoadNextCountry()
+        {
+            currentCountryIndex = random.Next(0, countries.Length);
+
+            string imagePath = countries_link[currentCountryIndex];
+            
+            countryPictureBox.Image = Image.FromFile(imagePath);
+
+            guessTextBox.Text = "";
+        }
+
+        private void CheckButton_Click(object sender, EventArgs e)
+        {
+            string guess = guessTextBox.Text.Trim();
+
+            if (guess.Equals(countries[currentCountryIndex], StringComparison.OrdinalIgnoreCase))
+            {
+                MessageBox.Show("Congratulations! You guessed it right.", "Guess Result", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                LoadNextCountry();
+            }
+            else
+            {
+                MessageBox.Show("Incorrect guess. Try again!", "Guess Result", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+            }
+        }
+    }
+
+    static class Program
+    {
+        [STAThread]
+        static void Main()
+        {
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            Application.Run(new MainForm());
+        }
+    }
+}
